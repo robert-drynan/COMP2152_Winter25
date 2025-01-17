74cea32587309754bf55f0bbd764ffd2c0a69bc6
robert-drynan <165441895+robert-drynan@users.noreply.github.com>
2025-01-17 08:53:24 -0500

    back up1

--- Week02/lab02.py
+++ Week02/lab02.py
@@ -1,15 +1,27 @@
 ﻿#Robert Drynan week 2
-from random import random
+import random
 
 choices = ["Rock", "Paper", "Scissors"]
-playerChoice = random.randint(0,2)
-computerChoice = random.randint(0,2)
-if playerChoice == computerChoice:
-    print("Draw")
-elif playerChoice == 0 & computerChoice == 2:
-    print("You win!")
-elif playerChoice == 1 & computerChoice == 2:
-    print("You lose!")
-elif playerChoice == 2 & computerChoice == 1:
-    print("You win!")
-elif playerChoice ==
+
+def main():
+    try:
+        choice = random.choice(choices)
+        user_input = input("Please enter your choice (Rock, Paper, Scissors): ").capitalize()
+        if user_input not in choices:
+            raise ValueError("Please enter a valid choice - Rock, Paper, Scissors")
+        player_choice = choices.index(user_input)
+        #random computer choice
+        computer_choice = random.randint(0,2)
+        print(f"player choice: {choices[player_choice]}")
+        print(f"computer choice: {choices[computer_choice]}")
+        #who wins
+        if computer_choice == player_choice:
+            print("Draw")
+        elif ((player_choice == 0 & computer_choice == 2) or
+              (player_choice == 1 & computer_choice == 0) or
+              (player_choice == 2 & computer_choice == 1)):
+            print("You win")
+        else:
+            print("You lose")
+    except ValueError as e:
+        print(f"Error: {e}")
