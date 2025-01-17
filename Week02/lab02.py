fe8b5f02bd806c26689ef3df9f7ec625515e743e
robert-drynan <165441895+robert-drynan@users.noreply.github.com>
2025-01-17 08:24:08 -0500

    first commit

---
+++ Week02/lab02.py
@@ -0,0 +1,15 @@
+﻿#Robert Drynan week 2
+from random import random
+
+choices = ["Rock", "Paper", "Scissors"]
+playerChoice = random.randint(0,2)
+computerChoice = random.randint(0,2)
+if playerChoice == computerChoice:
+    print("Draw")
+elif playerChoice == 0 & computerChoice == 2:
+    print("You win!")
+elif playerChoice == 1 & computerChoice == 2:
+    print("You lose!")
+elif playerChoice == 2 & computerChoice == 1:
+    print("You win!")
+elif playerChoice ==
