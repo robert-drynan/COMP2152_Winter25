cf9156c138ed9391d273b3788857ab64c00d2ff9
robert-drynan <165441895+robert-drynan@users.noreply.github.com>
2025-01-19 13:42:46 -0500

    lab 1 final

---
+++ Week01/lab1.py
@@ -0,0 +1,12 @@
+﻿#Sample Coding Questions 01 Week 01
+x = 1
+b = "blue"
+z = [1, 2, 3]
+
+
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+
+userAge = int(input("Please enter your age: ")) + 22
+print("Now showing the shop items filtered by age:", userAge)
